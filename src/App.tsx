58bad40d249a50d19043fb1ba61b841be74e06a7
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2021-12-29 19:08:38 +0330

    add cartesian board to the container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Point from "./components/point/Point";
 import GlobalState from "./components/global-state/GlobalState";
+import CartesianBoard from "./components/cartesian-board/CartesianBoard";
 
 function App() {
   return (
     <GlobalState>
+      <CartesianBoard />
       <Point isTargetPoint x={100} y={200} />
     </GlobalState>
   );
