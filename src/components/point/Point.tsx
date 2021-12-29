d5eb65938a257773162cb769812fb429e39d3f4f
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2021-12-29 21:43:51 +0330

    add event listeners

--- src/components/point/Point.tsx
+++ src/components/point/Point.tsx
@@ -1,27 +1,63 @@
-import { useContext } from "react";
-import { ActionTypes } from "../global-state/@types/GlobalStateTypes";
+import { useContext, useCallback, useEffect } from "react";
+import cartesianCoordinateToScreen from "../../utils/cartesianCoordinateToScreen";
+import screenCoordinateToCartesian from "../../utils/screenCoordinateToCartesian";
+import { ActionTypes, Point as PointType } from "../global-state/@types/GlobalStateTypes";
 import GlobalStateContext from "../global-state/GlobalStateContext";
+import styles from './point.module.css';
 
 interface Props {
-  x: number,
-  y: number,
-  isTargetPoint: boolean
+  point: PointType,
+  isTargetPoint: boolean,
+  index?: number,
 }
 
 export default function Point(props: Props) {
-  const { x, y, isTargetPoint } = props;
+  const dispatch = useContext(GlobalStateContext)[1];
+
+  const { x, y } = cartesianCoordinateToScreen(
+    props.point,
+    window.innerWidth,
+    window.innerHeight,
+  );
+
+  const left: string = `${(x / window.innerWidth) * 100}%`;
+  const top: string = `${(y / window.innerHeight) * 100}%`;
+
+  const mouseMoveListener = useCallback((e: MouseEvent) => {
+    const screenPoint: PointType = { x: e.clientX, y: e.clientY }
+    const cartesianPoint: PointType = screenCoordinateToCartesian(
+      screenPoint,
+      window.innerWidth,
+      window.innerHeight,
+    );
+    if (props.isTargetPoint) {
+      dispatch({
+        type: ActionTypes.TARGET_POINT,
+        payload: cartesianPoint,
+      });
+    }
+  }, [dispatch, props.isTargetPoint]);
+
+  useEffect(() => {
+    const mouseUpListener = () => {
+      window.removeEventListener('mousemove', mouseMoveListener);
+    };
+    window.addEventListener('mouseup', mouseUpListener);
+    return () => { window.removeEventListener('mouseup', mouseUpListener); }
+  }, [mouseMoveListener]);
 
   return (
     <div
+      className={styles.point}
       style={{
-        width: 10,
-        height: 10,
-        backgroundColor: isTargetPoint ? 'rgba(100, 100, 255)' : 'white',
+        backgroundColor: props.isTargetPoint ? 'rgba(100, 100, 255)' : 'white',
         position: 'absolute',
         transform: 'translate(-50%, -50%)',
-        left: x,
-        top: y,
-        borderRadius: '50%',
+        left,
+        top,
+      }}
+      onMouseDown={() => {
+        window.addEventListener('mousemove', mouseMoveListener);
       }}
     >
 
