e8b5d07119cd1fb9161e638585932e4f3cf694ba
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2021-12-29 11:16:49 +0330

    remove styles + add global state

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Point from "./components/point/Point";
+import GlobalState from "./components/global-state/GlobalState";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalState>
+      <Point isTargetPoint x={100} y={200} />
+    </GlobalState>
   );
 }
 
