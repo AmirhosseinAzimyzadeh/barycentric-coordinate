7ff26f05c4cbf00210b75fb251a1a1526577e689
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2021-12-30 11:19:49 +0330

    handle window resize

--- src/components/global-state/GlobalState.tsx
+++ src/components/global-state/GlobalState.tsx
@@ -1,6 +1,10 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
+import Debounce from "../../utils/Debounce";
+import { ActionTypes } from "./@types/GlobalStateTypes";
 import GlobalStateContext, { defaultStateValue } from "./GlobalStateContext";
 import mainReducer from "./reducers/mainReducer";
+
+const debounce = new Debounce();
 
 export default function GlobalState(props: React.PropsWithChildren<{}>) {
   const reducerValue = useReducer(
@@ -8,6 +12,16 @@ export default function GlobalState(props: React.PropsWithChildren<{}>) {
     defaultStateValue,
     () => { return defaultStateValue },
   );
+
+  useEffect(() => {
+    const listener = () => {};
+    window.addEventListener('resize',  () => {
+      debounce.use(() =>{
+        reducerValue[1]({ type: ActionTypes.RESET, payload: {x:0, y:0} });
+      });
+    });
+    return () => { window.removeEventListener('resize', listener); }
+  }, [reducerValue]);
 
   return (
     <GlobalStateContext.Provider value={reducerValue}>
