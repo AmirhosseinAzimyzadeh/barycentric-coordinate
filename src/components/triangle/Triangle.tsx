a68b83ac85f0764e3e72808d81195ca0eee4bb0e
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2021-12-30 11:20:11 +0330

    add lines component

--- src/components/triangle/Triangle.tsx
+++ src/components/triangle/Triangle.tsx
@@ -1,11 +1,13 @@
 import { useContext } from "react";
 import GlobalStateContext from "../global-state/GlobalStateContext";
+import Lines from "../lines/Lines";
 import Point from "../point/Point";
 
 export default function Triangle() {
   const [data] = useContext(GlobalStateContext);
   return (
     <>
+      <Lines />
       {data.triangle.map((p, index) => (
         <Point
           key={index}
