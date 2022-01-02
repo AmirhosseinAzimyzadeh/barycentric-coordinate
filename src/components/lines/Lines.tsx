c43f21ce2107b71124d5fedc57220a44aa00cf70
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-01-02 14:53:27 +0330

    add internal lines

--- src/components/lines/Lines.tsx
+++ src/components/lines/Lines.tsx
@@ -1,10 +1,17 @@
 import { useContext, useEffect, useRef, useState } from "react";
 import cartesianCoordinateToScreen from "../../utils/cartesianCoordinateToScreen";
+import findBarycentricCoordinate from "../../utils/findBarycentricCoordinate";
 import { Point } from "../global-state/@types/GlobalStateTypes";
 import GlobalStateContext from "../global-state/GlobalStateContext";
 
 export default function Lines() {
-  const points = useContext(GlobalStateContext)[0].triangle
+  const [data] = useContext(GlobalStateContext);
+  const targetPoint = cartesianCoordinateToScreen(
+    data.targetPoint,
+    window.innerWidth,
+    window.innerHeight,
+  );
+  const points = data.triangle
     .map((p) => (cartesianCoordinateToScreen(p, window.innerWidth, window.innerHeight)));
   const ref = useRef<HTMLCanvasElement>(null);
 
@@ -16,12 +23,12 @@ export default function Lines() {
     const ctx = ref.current.getContext("2d");
     const listener = () => {
       setSize([window.innerWidth, window.innerHeight]);
-      drawLines(points, ctx);
+      drawLines(points, targetPoint, ctx);
     };
-    drawLines(points, ctx);
+    drawLines(points, targetPoint, ctx);
     window.addEventListener('resize', listener);
     return () => { window.removeEventListener('resize', listener); }
-  }, [points]);
+  }, [points, targetPoint]);
 
   return (
     <canvas
@@ -32,7 +39,7 @@ export default function Lines() {
   );
 }
 
-function drawLines(points: Point[], ctx: CanvasRenderingContext2D | null) {
+function drawLines(points: Point[], targetPoint: Point, ctx: CanvasRenderingContext2D | null) {
   if (ctx === null) return;
   ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
   ctx.beginPath();
@@ -40,6 +47,33 @@ function drawLines(points: Point[], ctx: CanvasRenderingContext2D | null) {
   ctx.lineTo(points[1].x, points[1].y);
   ctx.lineTo(points[2].x, points[2].y);
   ctx.lineTo(points[0].x, points[0].y);
-  ctx.strokeStyle = 'white';
+  ctx.strokeStyle = 'rgba(255, 255, 255, 0.5)';
+  ctx.lineWidth = 2;
+  ctx.stroke();
+
+  // draw internal line if point is inside of triangle
+  const {a, b, c} = findBarycentricCoordinate(
+    targetPoint,
+    [points[0], points[1], points[2]], // triangle
+  );
+
+  const isInside = (a < 1 && a > 0)
+  && (b < 1 && b > 0)
+  && (c < 1 && c > 0);
+
+  if (!isInside) return;
+
+  ctx.strokeStyle = 'rgba(255, 255, 255, 0.2)';
+  ctx.lineWidth = 1;
+  ctx.moveTo(targetPoint.x, targetPoint.y);
+  ctx.lineTo(points[0].x, points[0].y);
+  ctx.stroke();
+
+  ctx.moveTo(targetPoint.x, targetPoint.y);
+  ctx.lineTo(points[1].x, points[1].y);
+  ctx.stroke();
+
+  ctx.moveTo(targetPoint.x, targetPoint.y);
+  ctx.lineTo(points[2].x, points[2].y);
   ctx.stroke();
 }
