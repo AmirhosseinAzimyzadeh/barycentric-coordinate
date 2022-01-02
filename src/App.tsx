fa1537bd1a18b58b1aaea4209e508aaf5fb31e1b
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-01-02 14:31:25 +0330

    wrap app to a container

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,21 @@ import InfoPanel from "./components/info-panel/InfoPanel";
 function App() {
   const [data] = useContext(GlobalStateContext);
   return (
-    <>
+    <div
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 1,
+        overflow: 'hidden',
+      }}
+    >
       <CartesianBoard />
       <Point isTargetPoint point={data.targetPoint} />
       <Triangle />
       <InfoPanel />
-    </>
+    </div>
   );
 }
 
